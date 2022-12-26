1b11775583eb459936996cdfa0b21c33a7ae09e0
laxamore <laxamore@gmail.com>
2022-12-26 09:12:08 +0700

    update mason

--- dotfiles/nvim/.config/nvim/lua/intellisense/mason/setup.lua
+++ dotfiles/nvim/.config/nvim/lua/intellisense/mason/setup.lua
@@ -1,9 +1,19 @@
 require('intellisense.mason.plugin')
 
--- local function setup()
-require("mason-lspconfig").setup({
-  ensure_installed = Servers,
-  automatic_installaion = true,
-})
--- end
--- pcall(setup)
+local function setup()
+  require("mason").setup({
+      ui = {
+          icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗"
+          }
+      }
+  })
+
+  require("mason-lspconfig").setup({
+    ensure_installed = Servers,
+    automatic_installaion = true,
+  })
+end
+pcall(setup)
